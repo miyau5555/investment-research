e16118999223dfa3aa79c688bb84e9da4dedd262
Shusuke Miyauchi <shusukemiyauchi@ShusukenoMacBook-Pro.local>
2021-03-21 20:35:25 +0900

    modify

---
+++ contrarian-simulation.py
@@ -0,0 +1,46 @@
+# coding: UTF-8
+import csv
+ 
+# 基準価格
+price = 0.0
+
+# 所持口数
+stock = 0
+# 毎月の購入金額
+wallet = 0.0
+payment = 1000.0
+ 
+# 出力するCSV
+output = ["年月,所持口数,総資産額"]
+
+# 前月の価格
+before = 0
+
+# csvのデータを読み込み
+with open('./data.csv') as f:
+    list = csv.reader(f)
+    num = 0;
+    for line in list:
+        num = num + 1
+        #一行目はヘッダのためスキップ
+        if num == 1:
+            # continueとは繰り返しのスキップで以降の処理をさせずに次の処理を行います。
+            continue
+ 
+        price = float(line[2])
+
+        if before > price :
+            #所持口数の追加
+            stock = stock + wallet / price
+            wallet = payment
+        else :
+            #価格が上がった場合は購入せず資金を貯める
+            wallet = wallet + payment
+        before = price
+        
+        #CSV１行分の作成
+        output.append(line[0] + "," + str(stock) + "," + str(stock * price + wallet))
+ 
+# 作成したリストを一行ずつcsvとして出力
+for line in output:
+  print(line)
--- dollar-cost-simulation.py
+++ dollar-cost-simulation.py
@@ -3,10 +3,7 @@ import csv
  
 # 基準価格
 price = 0.0
-# これまでにかかった信託報酬
-feeTotal = 0.0
-# 年率信託報酬
-feeRate = 0.03 / 100
+
 # 所持口数
 stock = 0
 # 毎月の購入金額
@@ -24,21 +21,13 @@ with open('./data.csv') as f:
         #一行目はヘッダのためスキップ
         if num == 1:
             # continueとは繰り返しのスキップで以降の処理をさせずに次の処理を行います。
-            continue 
+            continue
  
-        #コストの計算
-        #本来信託報酬は毎日引かれているものだが1年に１度精算という形にする
-        # float()とは文字列を小数点の数値型floatに変換する関数です。
-        # CSVから取り出したデータがString型のため小数点数値のfloatに変換しています。
-        if num % 12 == 0:
-          feeTotal = feeTotal + (float(line[1]) - feeTotal) * feeRate
- 
-        #本来の基準価格からこれまでかかったコストを引いていく
-        price = float(line[1]) - feeTotal
- 
+        price = float(line[1])
+
         #所持口数の追加
         stock = stock + wallet / price
-        
+
         #CSV１行分の作成
         output.append(line[0] + "," + str(stock) + "," + str(stock * price))
  
